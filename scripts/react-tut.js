38648945cd2865176dfac29256d9c48bb72681df
Bin Li <binblee@users.noreply.github.com>
2015-03-06 16:33:03 +0800

    Component Properties

--- scripts/react-tut.js
+++ scripts/react-tut.js
@@ -1,8 +1,21 @@
+var Comment = React.createClass({
+	render: function(){
+		return (
+			<div className="comment">
+				<h2 className="commentAuthor">
+					{this.props.author}
+				</h2>
+				{this.props.children}
+			</div>
+		);
+	}
+});
 var CommentList = React.createClass({
 	render: function(){
 		return (
 			<div className="commentList">
-				Hello world from a CommentList.
+				<Comment author="Michael">comment from michael</Comment>
+				<Comment author="James">comment from james</Comment>
 			</div>
 		);
 	}
