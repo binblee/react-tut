7f11eba9f9d5c152edbb6cdc4b603f9a128f1aed
Bin Li <binblee@users.noreply.github.com>
2015-04-01 17:30:21 +0800

    initial code for inbox using reactjs

---
+++ inbox/public/scripts/inbox.js
@@ -0,0 +1,9 @@
+var Inbox = React.createClass({
+    render: function(){
+        return(
+            <h1>Inbox</h1>
+        );
+    }
+});
+
+React.render(<Inbox />, document.getElementById('content'));
---
+++ inbox/server.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require 'webrick'
+require 'json'
+
+puts 'Server started: http://localhost:3000/'
+
+root = File.expand_path './public'
+server = WEBrick::HTTPServer.new :Port => 3000, :DocumentRoot => root
+
+    server.mount_proc '/emails.json' do |req, res|
+    products = JSON.parse(File.read('./emails.json'))
+
+    res['Content-Type'] = 'application/json'
+    res['Cache-Control'] = 'no-cache'
+    res.body = JSON.generate(products)
+end
+
+trap 'INT' do server.shutdown end
+
+server.start
